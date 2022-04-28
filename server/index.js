9ed5a7e8bf1e66fdecced88569e49bbcee414010
duvalbarrett <duvalbarrett@gmail.com>
2022-04-28 14:52:06 -0400

    everything added

--- server/index.js
+++ server/index.js
@@ -19,6 +19,10 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../index.css'))
+})
+
 let students = []
 app.post('/api/student', (req, res)=>{
     let {name} = req.body
@@ -38,6 +42,26 @@ app.post('/api/student', (req, res)=>{
     rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
 
     res.status(200).send(students)
+})
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
 })
 
 app.use(rollbar.errorHandler())
