8e9779affbfcc231c2d0fee9480f86b67c539187
duvalbarrett <duvalbarrett@gmail.com>
2022-04-28 14:43:42 -0400

    rollbar log info added

--- server/index.js
+++ server/index.js
@@ -13,9 +13,33 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
+
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+
+let students = []
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    res.status(200).send(students)
+})
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4004
 
