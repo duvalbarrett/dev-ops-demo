a092b5f31549763aafab7c4cebb5a13d1ec8a40a
duvalbarrett <duvalbarrett@gmail.com>
2022-04-28 14:46:38 -0400

    express json invoked

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+app.use(express.json())
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
