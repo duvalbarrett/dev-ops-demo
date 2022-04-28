142d0ed366554220ffc30692dbf4d6c7dd3fc268
duvalbarrett <duvalbarrett@gmail.com>
2022-04-28 15:56:42 -0400

    changes done

--- server/index.js
+++ server/index.js
@@ -19,9 +19,9 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-app.get('/css', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client/index.css'))
-})
+// app.get('/css', (req,res) => {
+//     res.sendFile(path.join(__dirname, '../client/index.css'))
+// })
 
 let students = []
 app.post('/api/student', (req, res)=>{
