cb1eb45fae24bd74fa6a5da973b80ffb915f253e
duvalbarrett <duvalbarrett@gmail.com>
2022-04-28 15:58:01 -0400

    change2

--- server/index.js
+++ server/index.js
@@ -16,12 +16,12 @@ rollbar.log('Hello world!')
 
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
-// app.get('/css', (req,res) => {
-//     res.sendFile(path.join(__dirname, '../client/index.css'))
-// })
+app.get('/css', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/index.css'))
+})
 
 let students = []
 app.post('/api/student', (req, res)=>{
